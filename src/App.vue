7ce3110f09fc5ce002d5ec37b27ef4754e81ae80
David <david.cedresx@gmail.com>
2020-11-29 19:22:13 -0400

    add demo

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <div id="app">
-    <hello-world msg="Welcome to Your Vue.js App" />
+  <div class="container my-6 has-text-centered">
+    <p class="title is-1 quantify has-text-primary">
+      Quantify
+    </p>
+    <p class="title is-1 sansation has-text-link">
+      Sansation
+    </p>
+    <p class="title is-1 metroclean has-text-danger">
+      Metroclean
+    </p>
+    <p class="title is-1 studio-revenge has-text-black">
+      Studio Revenge
+    </p>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.quantify {
+  font-family: quantify, serif;
+  font-weight: bold;
+}
+
+.sansation {
+  font-family: sansation, serif;
+  font-weight: bold;
+}
+
+.metroclean {
+  font-family: metroclean, serif;
+  font-weight: bold;
+}
+
+.studio-revenge {
+  font-family: "studio-revenge", serif;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+
 import '@pathscale/bulma-pull-2981-css-var-only'
 import '@pathscale/bulma-extensions-css-var'
+
+import '@pathscale/fonts-quantify'
+import '@pathscale/fonts-sansation'
+import '@pathscale/fonts-metroclean'
+import '@pathscale/fonts-studio-revenge'
 import './user.css'
+
+import App from './App.vue'
+
 
 createApp(App).mount('#app')
