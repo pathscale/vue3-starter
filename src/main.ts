0466a0bec7710fab9486494362e845872e3d1a33
Vladislav Dugintsov <vdugintsov@uma.tech>
2025-04-01 16:34:50 +0300

    Bulma replaced with Bulvar

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,8 @@ import {
   VAvatar,
 } from '@pathscale/vue3-ui'
 import App from './App.vue'
-import '@pathscale/bulma-pull-2981-css-var-only'
+import '@bulvar/bulma/css/bulma.css'
+import '~/assets/css/overrides/bulvar.css'
 import '@pathscale/bulma-extensions-css-var'
 import '@pathscale/fonts-metroclean'
 import { router } from './router'
